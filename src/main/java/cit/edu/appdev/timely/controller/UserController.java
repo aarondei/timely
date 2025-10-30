dc85fe422f33959f7e39a3ac92f526f88cab1413
giannakat <gkcarreon@gmail.com>
2025-10-30 22:47:35 +0800

    added user update and delete

--- src/main/java/cit/edu/appdev/timely/controller/UserController.java
+++ src/main/java/cit/edu/appdev/timely/controller/UserController.java
@@ -3,8 +3,11 @@ package cit.edu.appdev.timely.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +29,15 @@ public class UserController {
     @GetMapping("/getAllUser")
     public List<UserEntity> getAllStudents(){
         return user_service.getAllStudents();
+    }
+
+    @PutMapping("/updateUser/{id}")
+    public UserEntity updateUser(@PathVariable int id, @RequestBody UserEntity updatedUser){
+        return user_service.updateUser(id, updatedUser);
+    }
+
+    @DeleteMapping("deleteUser/{id}")
+    public String deleteUser(@PathVariable int id){
+        return user_service.deleteUser(id);
     }
 }
--- src/main/java/cit/edu/appdev/timely/service/UserService.java
+++ src/main/java/cit/edu/appdev/timely/service/UserService.java
@@ -20,4 +20,30 @@ public class UserService {
     public List<UserEntity> getAllStudents(){
         return user_repository.findAll();
     }
+
+    //U - update existing user details
+    public UserEntity updateUser(int id, UserEntity updatedUser){
+        UserEntity existingUser = user_repository.findById(id)
+        .orElseThrow(() -> new RuntimeException("User not found with id: " + id));
+
+        existingUser.setFirstname(updatedUser.getFirstname());
+        existingUser.setLastname(updatedUser.getLastname());
+        existingUser.setEmail(updatedUser.getEmail());
+        existingUser.setUsername(updatedUser.getUsername());
+        existingUser.setPassword(updatedUser.getPassword());
+
+        return user_repository.save(existingUser);
+    }
+
+    //D - delete user
+    public String deleteUser(int id){
+        if(!user_repository.existsById(id)){
+            throw new RuntimeException("User not found with id: " + id);
+        }
+
+        user_repository.deleteById(id);
+        return "User with ID" + id + "has been deleted successfully";
+    }
 }
+
+
