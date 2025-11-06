08c9d7380338c52ace3a37f757f666b78fa99c6a
Aaron <aaronvjatayna.gmail.com>
2025-11-06 15:15:39 +0800

    schedule CRUD

--- src/main/java/cit/edu/appdev/timely/controller/ScheduleController.java
+++ src/main/java/cit/edu/appdev/timely/controller/ScheduleController.java
@@ -3,13 +3,17 @@ package cit.edu.appdev.timely.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import cit.edu.appdev.timely.entity.ScheduleEntity;
+import cit.edu.appdev.timely.entity.UserEntity;
 import cit.edu.appdev.timely.service.ScheduleService;
 
 @RestController
@@ -27,5 +31,15 @@ public class ScheduleController {
     @GetMapping("/getAllSchedule")
     public List<ScheduleEntity> getAllSchedules() {
         return sched_service.getAllSchedules();
+    }
+
+    @PutMapping("/updateSchedule/{id}")
+    public ScheduleEntity updateSchedule(@PathVariable int id, @RequestBody ScheduleEntity updatedSchedule){
+        return sched_service.updateSchedule(id, updatedSchedule);
+    }
+
+    @DeleteMapping("deleteSchedule/{id}")
+    public String deleteSchedule(@PathVariable int id){
+        return sched_service.deleteSchedule(id);
     }
 }
--- src/main/java/cit/edu/appdev/timely/service/ScheduleService.java
+++ src/main/java/cit/edu/appdev/timely/service/ScheduleService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import cit.edu.appdev.timely.entity.ScheduleEntity;
+import cit.edu.appdev.timely.entity.UserEntity;
 import cit.edu.appdev.timely.repository.ScheduleRepository;
 
 @Service
@@ -26,5 +27,30 @@ public class ScheduleService {
     // READ from tblSchedule
     public List<ScheduleEntity> getAllSchedules() {
         return sched_repository.findAll();
+    }
+
+    
+    // UPDATE - update existing sched details
+    public ScheduleEntity updateSchedule(int id, ScheduleEntity updatedSchedule){
+        ScheduleEntity existingSchedule = sched_repository.findById(id)
+        .orElseThrow(() -> new RuntimeException("Schedule not found with id: " + id));
+
+        existingSchedule.setSection(updatedSchedule.getSection());
+        existingSchedule.setRoom(updatedSchedule.getRoom());
+        existingSchedule.setTeacher(updatedSchedule.getTeacher());
+        existingSchedule.setDay(updatedSchedule.getDay());
+        existingSchedule.setStudentNumber(updatedSchedule.getStudentNumber());
+
+        return sched_repository.save(existingSchedule);
+    }
+
+    // DELETE - delete sched
+    public String deleteSchedule(int id) {
+        if(!sched_repository.existsById(id)){
+            throw new RuntimeException("Schedule not found with id: " + id);
+        }
+
+        sched_repository.deleteById(id);
+        return "Schedule with ID" + id + "has been deleted successfully";
     }
 }
