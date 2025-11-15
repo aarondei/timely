24263bea6305058e8e43fc39f594e9d151581466
vncejnh <venicejuarez0809@gmail.com>
2025-11-15 09:20:14 +0800

    course CRUD

--- src/main/java/cit/edu/appdev/timely/controller/CourseController.java
+++ src/main/java/cit/edu/appdev/timely/controller/CourseController.java
@@ -3,13 +3,17 @@ package cit.edu.appdev.timely.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import cit.edu.appdev.timely.entity.CourseEntity;
+import cit.edu.appdev.timely.entity.ScheduleEntity;
 import cit.edu.appdev.timely.service.CourseService;
 
 @RestController
@@ -27,4 +31,16 @@ public class CourseController {
     public List<CourseEntity> getAllCourses() {
         return course_service.getAllCourses();
     }
+
+    @PutMapping("/updateCourse/{id}")
+    public CourseEntity updateCourse(@PathVariable int id, @RequestBody CourseEntity updatedCourse){
+        return course_service.updateCourse(id, updatedCourse);
+    }
+
+    @DeleteMapping("deleteCourse/{id}")
+    public String deleteCourse(@PathVariable int id){
+        return course_service.deleteCourse(id);
+    }
 }
+
+
